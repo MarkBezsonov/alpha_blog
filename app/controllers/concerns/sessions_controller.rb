3e1b5e1c2b293801d429317983a11edef6ddec2f
MarkBezsonov <markbezsonov@gmail.com>
2021-08-25 14:32:59 -0500

    Implemented sessions

--- app/controllers/concerns/sessions_controller.rb
+++ app/controllers/concerns/sessions_controller.rb
@@ -3,8 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:notice] = "You are now logged in. Welcome back!"
+      redirect_to user
+    else
+      flash.now[:alert] = "Your credentials were incorrect. Please try again."
+      render "new"
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:notice] = "You are now logged out. Come back soon!"
+    redirect_to root_path
   end
 end
