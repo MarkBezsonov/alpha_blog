266a77175e911159b143a73362ba5059cb6a7ad4
MarkBezsonov <markbezsonov@gmail.com>
2021-09-04 02:14:09 -0500

    Improved universal search and styling

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,8 @@
 class SearchController < ApplicationController
   def search
     q = params[:q]
-    @categories = Category.search(name_cont: q).result.order("created_at DESC").paginate(page: params[:pag_categories], per_page: 3)
-    @users = User.search(username_cont: q).result.order("created_at DESC").paginate(page: params[:pag_users], per_page: 3)
-    @articles = Article.search(title_or_description_cont: q).result.order("created_at DESC").paginate(page: params[:pag_articles], per_page: 3)
+    @categories = Category.search(name_cont: q).result.order("created_at DESC").paginate(page: params[:pag_categories], per_page: 5)
+    @users = User.search(username_cont: q).result.order("created_at DESC").paginate(page: params[:pag_users], per_page: 5)
+    @articles = Article.search(title_or_description_cont: q).result.order("created_at DESC").paginate(page: params[:pag_articles], per_page: 5)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,9 @@ module ApplicationHelper
     gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
     image_tag(gravatar_url, alt: user.username, class: "rounded shadow mx-auto d-block")
   end
+
+  def page_navigation_links(pages, param_name=:page)
+    will_paginate(pages, :params => {:anchor => ".search-top"}, :class => 'pagination', :inner_window => 2, :outer_window => 0, :renderer => BootstrapHelper::LinkRenderer, 
+    :previous_label => '&larr;'.html_safe, :next_label => '&rarr;'.html_safe, :param_name => param_name)
+  end
 end
